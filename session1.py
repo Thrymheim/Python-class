318567064d997011035ab0e75bdbc22fc7540ffb
Maziay <147552088+Thrymheim@users.noreply.github.com>
2024-10-17 18:55:06 +0330

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,40 @@
+print("Hello world")
+
+message = "Hello world"
+print(message)
+
+print(2+2)
+
+a = 2
+b = 3
+print(a+b)
+
+#You can comment using CTRL + ?
+
+first_name="amir"
+firstName="amir"
+f1="amir"
+
+firstName="amir hossein"
+print(firstName)
+print(firstName[3])
+print(firstName[4])
+print(firstName[-12])
+
+name="amir hossein"
+print(name.upper())
+
+ostad="ZOLFAGHARI"
+print(ostad.lower())
+
+address="babol karegar sardaran10"
+print(address.title())
+
+name="zolfaghari"
+print(f"hello {name}")
+
+a,b,c = 'amir','hossein','zolfaghari'
+print(f"{a.upper()} is friend with {b.lower()} and they both hate {c.title()}")
+
+print("Maziyar\nkolagar")
+print("Maziyar\tkolagar")
