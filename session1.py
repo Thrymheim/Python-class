8457a3e0b66009f5fe0af91db9f26cd31cec82b3
Maziay <147552088+Thrymheim@users.noreply.github.com>
2024-10-24 22:23:24 +0330

    session1.py

--- session1.py
+++ session1.py
@@ -11,29 +11,29 @@ print(a+b)
 
 #You can comment using CTRL + ?
 
-first_name="amir"
-firstName="amir"
-f1="amir"
+first_name="Maziyar"
+firstName="Maziyar"
+f1="Maziyar"
 
-firstName="amir hossein"
+firstName="Maziyar kolagar"
 print(firstName)
 print(firstName[3])
 print(firstName[4])
 print(firstName[-12])
 
-name="amir hossein"
+name="Maziyar kolagar"
 print(name.upper())
 
-ostad="ZOLFAGHARI"
+ostad="MAZIYAR"
 print(ostad.lower())
 
 address="babol karegar sardaran10"
 print(address.title())
 
-name="zolfaghari"
+name="Maziyar"
 print(f"hello {name}")
 
-a,b,c = 'amir','hossein','zolfaghari'
+a,b,c = 'amir','hossein','Maziyar'
 print(f"{a.upper()} is friend with {b.lower()} and they both hate {c.title()}")
 
 print("Maziyar\nkolagar")
